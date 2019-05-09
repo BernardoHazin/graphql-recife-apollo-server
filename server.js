db401323b8b38167fb21ecd210cad13b7d663f20
Bernardo hazin <bernardo.hazin@gmail.com>
2019-05-09 17:17:02 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const server = require('vue-cli-plugin-apollo/graphql-server')
+
+const opts = {
+  host: 'localhost',
+  port: process.env.PORT || 4000,
+  graphqlPath: '/graphql',
+  subscriptionsPath: '/graphql',
+  enableMocks: false,
+  enableEngine: false,
+  cors: process.env.cors || '*',
+  timeout: 1000000,
+  quiet: true,
+  paths: {
+    typeDefs: require.resolve('./apollo-server/type-defs.js'),
+    resolvers: require.resolve('./apollo-server/resolvers.js'),
+    context: require.resolve('./apollo-server/context.js'),
+    directives: require.resolve('./apollo-server/directives.js'),
+    dataSources: require.resolve('./apollo-server/data-sources.js')
+  }
+}
+
+server(opts, () => {
+  console.log('Apollo server is running!')
+})
