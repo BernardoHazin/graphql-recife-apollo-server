76fa75192c8b079377bc4a381734b3e2dcf02cd5
hazin <bernardo.hazin@mesainc.com.br>
2019-07-10 14:51:54 -0300

    Adicionando contexto a instância do ApolloSever

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const typeDefs = require('./apollo-server/type-defs')
 const resolvers = require('./apollo-server/resolvers')
 const { ApolloServer } = require('apollo-server')
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+  typeDefs /* GraphQL Schema Definition Language */,
+  resolvers /* Required! */,
+  context: async ({ req, connection }) => {
+    return { title: `I'm the context!` }
+  }
+})
 
 server
   .listen({ port: process.env.PORT || 4000, subscriptions: 'subscriptions' })
